1721c0c7459a07c22244776722e17749a45e762b
Ani Shakhkyan <shakhkiyan.ani@gmail.com>
2021-05-24 01:07:05 +0400

    dynamic alocation

--- hw.cpp
+++ hw.cpp
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 int main()
 {
-	int *opened, res;
+	int *opened, res, count = 1,a;
 	FILE *data;
-	
+	opened = &a;
 	res = 0;
 	data = fopen("input.txt","r");
-	opened = (int*)malloc(150*sizeof(int));
+	
+	while(fscanf(data, "%d", opened)!=EOF){
+	   	count++;
+	}
+	opened = (int*)malloc(count*sizeof(int));
+	data = fopen("input.txt","r");
 	while (!data)
 	{
 		printf("Error");
 		break;
 	}
 	while (fscanf(data, "%d", opened)!=EOF)
-	{ 
-	res = res + *opened;	}
+		res = res + *opened;
 	printf("Sum in input.txt is %d\n",res);
-	fclose(data);
 	data = fopen("output.txt", "w+");
 	fprintf(data, "%d\n", res);
 	fclose(data);
-	return (res);
+	free(opened);
 }
